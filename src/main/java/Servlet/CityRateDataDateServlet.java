28c5f99f6f61741391c6fecc526888ccec070593
umagnus <935232304@qq.com>
2017-09-07 23:30:24 -0700

    add cityRateDataDateServelt

--- src/main/java/Servlet/CityRateDataDateServlet.java
+++ src/main/java/Servlet/CityRateDataDateServlet.java
@@ -7,9 +7,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Date;
 import java.util.List;
 
+import com.alibaba.fastjson.JSON;
 import model.CityRateData;
 import util.*;
 
@@ -18,9 +20,30 @@ import util.*;
  */
 public class CityRateDataDateServlet extends HttpServlet{
 
+    protected void getJson(HttpServletRequest request,HttpServletResponse response,Object object){
+        //response.setContentType("text/html;charset=UTF-8");
+        //禁用缓存，确保网页信息是最新数据
+        response.setHeader("Pragma","No-cache");
+        response.setHeader("Cache-Control","no-cache");
+        response.setDateHeader("Expires", -10);
+        PrintWriter out = null;
+        try {
+            out = response.getWriter();
+            String jsonStr= JSON.toJSONString(object);
+            out.print(jsonStr);
+            out.flush();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }finally{
+            out.close();
+        }
+    }
+
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException ,IOException{
         doPost(request,response);
+        //response.getWriter().write("111");
     }
 
     public void doPost(HttpServletRequest request,HttpServletResponse response)
@@ -37,6 +60,12 @@ public class CityRateDataDateServlet extends HttpServlet{
         }catch(Exception e){
             e.printStackTrace();
         }
-        
+        java.sql.Date date= java.sql.Date.valueOf("2010-12-01");
+        CityRateData cd=new CityRateData();
+        cd.setCityRateData(date,"南京",10,20,0.5);
+        crdlist.add(cd);
+        getJson(request,response,crdlist);
     }
+
+
 }
