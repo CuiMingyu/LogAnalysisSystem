26f10f09dac7326f34ae671e6d83459a1daaee86
umagnus <935232304@qq.com>
2017-09-12 00:16:11 -0700

    add mostPV DAO and Service

---
+++ src/main/java/service/MostPVService.java
@@ -0,0 +1,28 @@
+package service;
+
+import dao.MostPVDAO;
+import model.MostPV;
+import util.Sqldb;
+
+import java.sql.Connection;
+import java.util.List;
+
+/**
+ * Created by yxy on 9/12/17.
+ */
+public class MostPVService {
+    //前端给查询数字，返回相应数量的PV排序值 number<=20
+    public List<MostPV> SelectByCityName(int number) {
+        Connection conn=null;
+        List<MostPV> list=null;
+        try {
+            conn = Sqldb.getDefaultConnection();
+            list = MostPVDAO.GetMostPV(conn, number);
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally {
+            Sqldb.closeConnection(conn);
+        }
+        return list;
+    }
+}
