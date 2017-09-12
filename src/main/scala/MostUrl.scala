080072b22abea496042c3762227c442545f5c9ea
Psychim <604315924@qq.com>
2017-09-12 02:59:48 +0000

    Scala

--- src/main/scala/MostUrl.scala
+++ src/main/scala/MostUrl.scala
@@ -2,8 +2,9 @@
   * Created by yxy on 9/11/17.
   */
 import org.apache.spark.sql.SparkSession
+import org.apache.hadoop.fs._
 
-object MostUrl {
+object MostUrl{
   def main(args:Array[String]):Unit={
     val spark= SparkSession.builder().appName("MostUrl").master("local").enableHiveSupport().getOrCreate()
     val sc=spark.sparkContext
@@ -12,12 +13,13 @@ object MostUrl {
       "Device","Devmac","Apmac","Acmac","Url",
       "Response","Uplink_packets","Uplink_flow","Downlink_packets","Downlink_flow")
     val bcfields=sc.broadcast(fields)
-
-    val data=sc.textFile("hdfs://yxy:9000/user/root/input/data").map(_.split("\t"))
+    val inputPath="hdfs://scm001:9000/user/hive/warehouse/loganalysis.db/log"
+    //val hdfs=FileSystem.get(new java.net.URI(inputPath),new Configuration())
+    val data=sc.textFile(inputPath).map(_.split("\t"))
     data.map{m=>
-      val url=m(bcfields.value.indexOf("URL"))
-      val weburl=url.substring(0,url.indexOf('/',3))
+      val url=m(bcfields.value.indexOf("Url"))
+      val weburl=url.split('/')(2)
       weburl
-    }.distinct().map(m=>(m(1),1)).reduceByKey(_+_).foreach(m=>println(m._1+"url num:"+m._2))
+    }.map(m=>(m,1)).reduceByKey(_+_).foreach(m=>println(m._1+" url num:"+m._2))
   }
 }
