a35c53a979a8ff2b5c20653866d758bb5f7bd020
umagnus <935232304@qq.com>
2017-09-12 00:29:04 -0700

    add mostUV DAO and Service

---
+++ src/main/java/model/MostUV.java
@@ -0,0 +1,33 @@
+package model;
+
+/**
+ * Created by yxy on 9/12/17.
+ */
+public class MostUV {
+    private String urlname=null;
+    private int UV=0;
+
+    public void setMostUV(String urlname,int UV){
+        this.urlname=urlname;
+        this.UV=UV;
+    }
+    public String getUrlname() {
+        return urlname;
+    }
+
+    public void setUrlname(String urlname) {
+        this.urlname = urlname;
+    }
+
+    public int getUV() {
+        return UV;
+    }
+
+    public void setUV(int UV) {
+        this.UV = UV;
+    }
+
+    public String toString(){
+        return urlname+'\t'+String.valueOf(UV);
+    }
+}
--- src/main/java/service/MostUVService.java
+++ src/main/java/service/MostUVService.java
@@ -1,7 +1,8 @@
 package service;
 
 import dao.MostPVDAO;
-import model.MostPV;
+import dao.MostUVDAO;
+import model.MostUV;
 import util.Sqldb;
 
 import java.sql.Connection;
@@ -10,14 +11,14 @@ import java.util.List;
 /**
  * Created by yxy on 9/12/17.
  */
-public class MostPVService {
-    //前端给查询数字，返回相应数量的PV排序值 number<=20
-    public List<MostPV> GetMostPV(int number) {
+public class MostUVService {
+    //前端给查询数字，返回相应数量的UV排序值 number<=20
+    public List<MostUV> GetMostUV(int number) {
         Connection conn=null;
-        List<MostPV> list=null;
+        List<MostUV> list=null;
         try {
             conn = Sqldb.getDefaultConnection();
-            list = MostPVDAO.GetMostPV(conn, number);
+            list = MostUVDAO.GetMostUV(conn, number);
         }catch(Exception e){
             e.printStackTrace();
         }finally {
