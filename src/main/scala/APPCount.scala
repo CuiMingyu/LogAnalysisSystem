499fc5402881f1959e7d02fd68cc4debcd22998a
umagnus <935232304@qq.com>
2017-09-13 06:02:16 -0700

    APPCount

--- src/main/scala/APPCount.scala
+++ src/main/scala/APPCount.scala
@@ -2,6 +2,8 @@ package main.scala
 
 import org.apache.spark.sql.SparkSession
 
+
+
 import scala.collection.mutable
 
 /**
@@ -18,6 +20,7 @@ object APPCount {
       (appname,byte)
     }.reduceByKey(_+_).map(m=>(m._2,m._1)).sortByKey(ascending=false)
     appdata.foreach(m=>println(m._2+" user num:"+m._1))
+
     val test:Map[String,String]=Map("changba"->"K歌","12306"->"出行","suanya"->"出行",
     "ctrip"->"旅游","qunar"->"旅游","jd"->"购物","tmall"->"购物","meiyan"->"拍照",
     "poco"->"拍照","qq"->"社交","wechat"->"社交","inke"->"直播","yy"->"直播","douyu"->"直播")
@@ -27,5 +30,43 @@ object APPCount {
       (labelname,byte)
     }.reduceByKey(_+_).map(m=>(m._2,m._1)).sortByKey(ascending=false)
     labeldata.foreach(m=>println(m._2+" user num:"+m._1))
+
+    val usernum=data.map{m=>
+      val labelname=test.apply(m(3))
+      val user=m(2)
+      (labelname,user)
+    }.distinct().map(m=>(m._1,1)).reduceByKey(_+_)
+    usernum.foreach(m=>println(m))
+
+    val userdata=data.map{m=>
+      val labelname=test.apply(m(3))
+      val user=m(2)
+      val byte=m(5).toInt+m(6).toInt
+      ((labelname,user),byte)
+    }.reduceByKey(_+_).map(m=>(m._2,m._1)).sortByKey(ascending=false)
+    userdata.foreach(m=>println(m._2+" user num:"+m._1))
+
+    val list=List("社交","K歌","旅游","直播","出行","购物","拍照")
+    for(elem <- list){
+      println(elem+":")
+      val array=usernum.filter(m=>m._1==elem).map(m=>m._2).collect
+      val low=(array(0).toDouble/3).toInt
+      val high=(array(0).toDouble/3*2).toInt
+      val labelcount=userdata.filter(m=>m._2._1==elem).sortByKey(ascending = false)
+      for(i<-0 to labelcount.collect.length-1){
+        if(i>=high-1) {
+          val printlabel=labelcount.collect.apply(i)
+          println("用户"+printlabel._2._2+"是高频用户")
+        }
+        else if(i<low-1){
+          val printlabel=labelcount.collect.apply(i)
+          println("用户"+printlabel._2._2+"是低频用户")
+        }
+        else{
+          val printlabel=labelcount.collect.apply(i)
+          println("用户"+printlabel._2._2+"是中频用户")
+        }
+      }
+    }
   }
 }
