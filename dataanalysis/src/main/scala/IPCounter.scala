5d8ddcafd5c3ebf5fdedc55b6ebe236f327cabf3
Psychim <604315924@qq.com>
2017-09-14 08:40:00 +0000

    moved

---
+++ dataanalysis/src/main/scala/IPCounter.scala
@@ -0,0 +1,27 @@
+package main.scala
+import org.apache.spark.sql.SparkSession
+import main.scala.util.UrlUtil
+
+/**
+  * Created by root on 9/12/17.
+  */
+object IPCounter {
+  def run(inputPath:String,outputPath:String,num: Int){
+    val spark= SparkSession.builder().appName("MostUrl").master("local").enableHiveSupport().getOrCreate()
+    val sc=spark.sparkContext
+    val bcfields=sc.broadcast(Global.fields)
+    val data=sc.textFile(inputPath).map(_.split("\t"))
+    //counts the IP of each hosts and sorts by it, then take the top [num] (host,IP) pairs
+    val ipcounts=data.map{m=>
+      val url=m(bcfields.value.indexOf("Url"))
+      val ip=m(bcfields.value.indexOf("IP"))
+      (UrlUtil.getHostName(url),ip)
+    }.distinct().map(m=> (m._1,1)).reduceByKey(_+_).map(m=>(m._2,m._1)).sortByKey(ascending=false).take(num)
+    //save on hdfs
+    sc.makeRDD(ipcounts).map(m=> m._2+"\t"+m._1).repartition(1).saveAsTextFile(outputPath)
+  }
+  def main(args:Array[String]): Unit ={
+    run("hdfs://scm001:9000/user/hive/warehouse/loganalysis.db/log",
+      "hdfs://scm001:9000/LogAnalysisSystem/IPOutput",20)
+  }
+}
---
+++ dataanalysis/src/main/scala/PVCounter.scala
@@ -0,0 +1,27 @@
+/**
+  * Created by yxy on 9/11/17.
+  */
+package main.scala
+import org.apache.spark.sql.SparkSession
+import main.scala.util.UrlUtil
+
+object PVCounter {
+  def run(inputPath:String,outputPath:String,num: Int){
+    val spark= SparkSession.builder().appName("MostUrl").master("local").enableHiveSupport().getOrCreate()
+    val sc=spark.sparkContext
+    val bcfields=sc.broadcast(Global.fields)
+
+    val data=sc.textFile(inputPath).map(_.split("\t"))
+    //counts the PV of each hosts and sorts by it, then take the top [num] (host,PV) pairs
+    val pvcounts=data.map{m=>
+      val url=m(bcfields.value.indexOf("Url"))
+      UrlUtil.getHostName(url)
+    }.map(m=>(m,1)).reduceByKey(_+_).map(m=>(m._2,m._1)).sortByKey(ascending=false).take(num)
+    //save on hdfs
+    sc.makeRDD(pvcounts).map(m => m._2+"\t"+m._1).repartition(1).saveAsTextFile(outputPath)
+  }
+  def main(args:Array[String]):Unit={
+    run("hdfs://scm001:9000/user/hive/warehouse/loganalysis.db/log",
+      "hdfs://scm001:9000/LogAnalysisSystem/PVOutput",20)
+  }
+}
